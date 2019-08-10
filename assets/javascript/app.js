1a6042e4f8cc4970906145b3e7d92cf0693a763b
rawrgoesaimee <rawrgoesaimee@hotmail.com>
2019-08-10 13:51:43 -0700

    Added javascript and html code

---
+++ assets/javascript/app.js
@@ -0,0 +1,15 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDt9EfSxQ7dUZbRfOvpJCbbAhS-S0q6UOA",
+    authDomain: "traintime-2bb84.firebaseapp.com",
+    databaseURL: "https://traintime-2bb84.firebaseio.com",
+    projectId: "traintime-2bb84",
+    storageBucket: "",
+    messagingSenderId: "872420135779",
+    appId: "1:872420135779:web:38d8627d8af79e36"
+  };
+
+  firebase.initializeApp(firebaseConfig);
+
+  let database = firebase.database();
+
+  
