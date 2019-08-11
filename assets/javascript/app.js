c2e954b9c4bff7a02a03c69aae792c718f410cb9
rawrgoesaimee <rawrgoesaimee@hotmail.com>
2019-08-11 11:47:29 -0700

    added final touches to js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,47 +3,45 @@ const firebaseConfig = {
     authDomain: "traintime-2bb84.firebaseapp.com",
     databaseURL: "https://traintime-2bb84.firebaseio.com",
     projectId: "traintime-2bb84",
-    storageBucket: "",
+    storageBucket: "https://traintime-2bb84.firebaseio.com/",
     messagingSenderId: "872420135779",
     appId: "1:872420135779:web:38d8627d8af79e36"
   };
-
   firebase.initializeApp(firebaseConfig);
-
   let database = firebase.database();
-
   let name = ""
   let destination = ""
   let trainTimes = ""
   let reccurence = ""
-
-  $("#submit").on("click", function(event){
+  $("#submit-train").on("click", function(event){
     event.preventDefault();
     name = $("#train-name").val().trim()
     destination = $("#destination").val().trim()
-    trainTimes = ($("#first-train").val().trim(), "hh:mm").subtract(1, "years").format("X")
+    trainTimes = moment($("#first-train").val().trim(),"HH:mm").subtract(1,"years").format("X")
     reccurence = $("#frequency").val().trim()
-
-  let name = {
-      name: name,
-      destination: destination,
-      trainTimes: trainTimes,
-      reccurence: reccurence
-  };
-
+    database.ref().push({
+        name: name,
+        destination: destination,
+        trainTimes: trainTimes,
+        reccurence: reccurence
+    });
     $("#train-name").val("")
     $("#destination").val("")
     $("#first-train").val("")
     $("#frequency").val("")
-
   });
-
   database.ref().on("child_added", function(snapshot){
-
     $("#train-name").text(snapshot.val().name)
     $("#destination").text(snapshot.val().destination)
     $("#first-train").text(snapshot.val().trainTimes)
     $("#frequency").text(snapshot.val().recurrence)
+
+    let remainder = moment().diff(moment(trainTimes,"X"),"minutes") % reccurence
+    let minutes = reccurence - remainder
+    let arrival = moment().add(minutes,"minutes").format("hh:mm A")
+    let newTr = $("<tr>")
+    newTr.html(`<td>${name}</td><td>${destination}</td><td>${reccurence}</td><td>${arrival}</td><td>${minutes}</td>`)
+    $("#main-holder").append(newTr)
+    
     
   })
-  
